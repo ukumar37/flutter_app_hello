675bb2585a9e50f5a86c96d9e452110dcec57487
Uday Kumar <udaydotkumar@gmail.com>
2020-07-07 13:27:41 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+
+void main() {
+
+  runApp(
+      Center(
+        child:  Text(
+          "Hello Uday. Welcome to Flutter!",
+          textDirection: TextDirection.ltr
+        ),
+      )
+  );
+}
